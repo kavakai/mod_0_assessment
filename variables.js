a5dc19084c2878c8afe2e1ac0d3edbe927545fe4
Kai Kuller <kavakai@gmail.com>
2021-09-03 12:41:01 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,12 @@
+var movie = "Dumb and Dumber";
+var isEntertaining = true;
+movie = "The Darjeeling Limited";
+
+var num4 = 4;
+var num6 = 6;
+var product = num4 * num6;
+
+var favoriteFoods = ["Enchiladas", "Pizza", "Ice cream"];
+favoriteFoods.shift();
+
+console.log (movie.length);
