15d61f51f020d6d345ff673d44ee0301d7874053
Kai Kuller <kavakai@gmail.com>
2021-09-03 12:49:46 -0600

    Add dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,9 @@
+var dinosaur = {
+  size: "Vary Large",
+  color: "Brown",
+  heightInFeet: 22,
+  lengthInFeet: 37,
+  eatsMeat: true,
+  playsWellWithOthers: false,
+  favoriteFoods: ["Other dinosaurs", "Rocks", "Human-like creatures"],
+}
