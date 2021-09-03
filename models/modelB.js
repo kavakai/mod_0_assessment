6ddd50dcb6da7005690d344f369669f1646acb64
Kai Kuller <kavakai@gmail.com>
2021-09-03 12:59:48 -0600

    Add developer object

---
+++ models/modelB.js
@@ -0,0 +1,12 @@
+var developer = {
+  name: "Kai Kuller",
+  location: "Santa Fe, NM",
+  age: 38,
+  typingWordsPerMinute: 63,
+  hireable: true,
+  funny: true,
+  willingToRelocate: false,
+  favoriteCodeLearningSources: ["Turing", "Udemy", "Friends"],
+  skills: ["Javascript", "Customer service", "Team-player", "Hard worker", "Quick learner"],
+
+}
